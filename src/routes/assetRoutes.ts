74812ad111e500551856c70eb1d24e8b5e2655e5
Pushkar Mondal <nishitm060@gmail.com>
2025-09-09 19:31:29 +0530

    More routes for Assets page

--- src/routes/assetRoutes.ts
+++ src/routes/assetRoutes.ts
@@ -91,6 +91,55 @@ router.get("/api/getAsset/:id", authMiddleWare, async(req, res) => {
     }
 })
 
+router.put("/api/updateAsset/:id", authMiddleWare, async(req, res) => {
+    try {
+        const assetId = req.params.id;
+        const {title, description, type, game, rarity, style, modType, tags, 
+            price, discountPrice, thumbnail, previewUrl, fileUrl, fileSize, 
+            status, isFeatured, version} = req.body;
+        if(!assetId){
+            return res.status(400).json({error: "Asset ID is required!"})
+        }
+        const updateAsset = await prisma.asset.update({
+            where: {
+                id: assetId
+            }, data: {
+                title, description, type, game, rarity, style, modType, tags,
+                metadata: {}, price, discountPrice, thumbnail, previewUrl, fileUrl, fileSize,
+                status, isFeatured, version, creatorId: req.user.id
+            },
+            select: {
+                id: true, title: true, description: true, type: true, game: true,
+                rarity: true, style: true, modType: true, tags: true, metadata: true,
+                price: true, discountPrice: true, thumbnail: true, previewUrl: true,
+                fileUrl: true, fileSize: true, status: true, isFeatured: true,
+                version: true, creatorId: true, createdAt: true,
+            }
+        })
+        return res.status(200).json({message: "Asset updated successfully!", asset: updateAsset})
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({error: "Internal server error"})
+    }
+})
+
+router.delete("/api/deleteAsset/:id", authMiddleWare, async(req, res) => {
+    try {
+        const assetId = req.params.id;
+        if(!assetId){
+            return res.status(400).json({error: "Asset ID is required!"})
+        }
+        const deleteAsset = await prisma.asset.delete({
+            where: {
+                id: assetId
+            }
+        })
+        return res.status(200).json({message: "Asset deleted successfully!"})
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({error: "Internal server error"})
+    }
+})
 export default router
 
     
